2f61a200d94a773f5a623158db83d2fd169e46fd
jszufa <118739912+jszufa@users.noreply.github.com>
2023-02-28 15:52:59 +0100

    getNextPosts gotowe

--- src/views/Home.js
+++ src/views/Home.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import axios from "axios";
 import Post from '../components/Post';
 
+import "./Home.css"
+
 function Home() {
 
     const [posts, setPosts] = useState([]);
@@ -18,18 +20,35 @@ function Home() {
 
     }
 
-    console.log(posts);
+
+    const getNextPosts = () => {
+
+        axios.post(`https://akademia108.pl/api/social-app/post/older-then`,
+            { "date": posts[posts.length - 1].created_at }
+        )
+            .then((response) => {
+
+                let olderPosts = response.data;
+                return (setPosts(posts.concat(olderPosts)));
+                //czy return jest tutaj w ogóle potrzebne?
+            });
+
+    }
+
+
 
 
 
     return (
 
         <div className='postList'>
-             <h2>Home</h2>
-            {posts.map((post)=>{
+            <h2>Home</h2>
+            {posts.map((post) => {
                 return (
                     <Post post={post} key={post.id} />
-            )})}
+                )
+            })}
+            <button className='loadMoreBtn' onClick={() => { getNextPosts() }}>Load more</button>
         </div>
     )
 }
